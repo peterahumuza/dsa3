ef8621221bddd25fc8630e8645dbfabe89aaae19
Peter Ahumuza <peter@172-29-104-133.w.africa.local.cmu.edu>
2024-03-07 22:19:18 +0200

    Changes to be committed: 	modified:   TextAnalysisImplBST.cpp

--- TextAnalysisImplBST.cpp
+++ TextAnalysisImplBST.cpp
@@ -30,31 +30,81 @@ using namespace std;
 
 #define andrewID "pahumuza"
 
-struct word_properties
-{
-   int ascii_val;
-   char word[100];
-};
-
-
-int covert_to_ascii(char word[],int wordLength){
-    int total,i =0;
-
-    for(int i =0; i < wordLength; i++)
-    {
-        total += static_cast<int>(word[i]);
-    }
-
-    return total;
-}
 
 // declare tree node with data, right and left child
 struct node
 {
     struct node *rightChild; // right child
     char data[100]; // data 
+    int count;
     struct node *leftChild; // left child
 };
+
+
+struct node *root =nullptr; // initialize the root node
+
+// define insertNode method that inserts a node on the tree
+void insertNode(char A[])
+{
+    //declare and initialize current node pointer to root node
+    struct node *current = root;
+    struct node *previous, *newNode; //declare previous and newNode
+    int compareResult;
+
+    //check if root is a nullptr
+    if(root == nullptr)
+    {
+        newNode = new node; // create a new node
+        memset(newNode->data, '\0', sizeof(newNode->data));
+        strcpy(newNode->data, A); // set it's data
+        newNode->count = 1;
+        newNode->leftChild = newNode->rightChild = nullptr; // set the left and right child to null
+
+        root = newNode; // set root to point to the newNode
+
+        return; // exit the method
+    }
+
+    while(current != nullptr)
+    {
+        // set previous equal to previous
+        previous = current;
+
+        compareResult = strcmp(A,current->data);
+
+        if(compareResult < 0)
+        {
+            current = current->leftChild;
+        }
+        else if(compareResult > 0)
+        {
+            current = current->rightChild;
+        }
+        else
+        {
+            current->count++;
+            return;
+        }
+    }
+
+    newNode = new node; // create a new node
+    memset(newNode->data, '\0', sizeof(newNode->data));
+    strcpy(newNode->data, A); // set it's data
+    newNode->leftChild = nullptr; // set left child to a null pointer
+    newNode->rightChild = nullptr; // set right child to a null pointer
+    newNode->count = 1;
+
+    compareResult = strcmp(A,previous->data);
+
+    if(compareResult < 0){
+        previous->leftChild = newNode;
+    }
+    else
+    {
+        previous->rightChild = newNode;
+    }
+}
+
 
 /**
  * convert word to lower case and terminate when you get to the ' character
@@ -73,6 +123,16 @@ void process_word(char A[]){
             A[i] = '\0';
             return;
         }
+    }
+}
+
+//recursive inorder traversal
+void Inorder(node *p,int i) {
+    if (p){
+        Inorder(p->leftChild,i+1); // recursive call
+        cout << p->data <<" " << p->count << " (" << i <<")"<< endl; // output to screen
+        // fprintf(f, "%.0f %.0f %.0f \n", p->data, p->data, p->data); // write to file
+        Inorder(p->rightChild,i+1); // recursive call
     }
 }
 
@@ -119,10 +179,15 @@ int main(int argc, char *argv[]){
         while(fscanf(inputFile, "%s", &word) != EOF)
         {
             process_word(word);
-            cout << word << endl;
+            
+            insertNode(word);
+            // cout << word << endl;
         }
 
         fclose(inputFile);
+
+        Inorder(root,0);
+        root = nullptr;
 
     }
 
